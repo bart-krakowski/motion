08c36c9a36d5c62e477a09cc5bd07a3016b0fec6
Bart Krakowski <bartlomiej@krakowski.dev>
2022-06-03 11:58:42 +0200

    fix: typing of the AnimateSharedLayout component

--- packages/framer-motion/src/components/AnimateSharedLayout.tsx
+++ packages/framer-motion/src/components/AnimateSharedLayout.tsx
@@ -4,9 +4,9 @@ import { useConstant } from "../utils/use-constant"
 import { LayoutGroup } from "./LayoutGroup"
 
 let id = 0
-export const AnimateSharedLayout: React.FunctionComponent = ({
+export const AnimateSharedLayout: React.FunctionComponent<React.PropsWithChildren<{}>> = ({
     children,
-}: React.PropsWithChildren<{}>) => {
+}) => {
     React.useEffect(() => {
         warning(
             false,
